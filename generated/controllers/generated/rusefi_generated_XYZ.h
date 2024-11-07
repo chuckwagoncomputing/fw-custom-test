97b084afc8c237fe9397f7b966ea25b29dfd3525
GitHub git update Action <action@github.com>
2024-11-07 16:49:55 +0000

    Auto-generated Configs
    [no ci]

--- generated/controllers/generated/rusefi_generated_XYZ.h
+++ generated/controllers/generated/rusefi_generated_XYZ.h
@@ -1667,7 +1667,7 @@
 #define ts_show_vr_threshold_all false
 #define ts_show_vr_threshold_pins true
 #define ts_show_vvt_output true
-#define TS_SIGNATURE "rusEFI main.2024.11.06.XYZ.1131481494"
+#define TS_SIGNATURE "rusEFI main.2024.11.07.XYZ.1131481494"
 #define TS_SIMULATE_CAN '>'
 #define TS_SIMULATE_CAN_char >
 #define TS_SINGLE_WRITE_COMMAND 'W'
